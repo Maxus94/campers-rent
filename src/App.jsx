4b4c6d422046e9569e8494b4dd56fa2496830327
Maxus94 <kmaxusk@gmail.com>
2024-04-12 21:57:09 +0300

    routes
    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import CampersCatalog from "./pages/CampersCatalog/CampersCatalog";
+import CampersFavorites from "./pages/CampersFavorites/CampersFavorites";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-      </Route>
+    <Routes>      
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<CampersCatalog />} />
+        <Route path="/favorites" element={<CampersFavorites />} />        
+        <Route path="*" element={<Home />} />        
     </Routes>
   );
 }
-
 export default App;
