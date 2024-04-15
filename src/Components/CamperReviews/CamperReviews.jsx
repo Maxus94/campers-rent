15ba88f64c559feb9ab8b43ca03609c165c5ca73
Maxus94 <kmaxusk@gmail.com>
2024-04-15 09:57:12 +0300

    reviews changed

--- src/Components/CamperReviews/CamperReviews.jsx
+++ src/Components/CamperReviews/CamperReviews.jsx
@@ -1,9 +1,30 @@
+import { useSelector } from "react-redux";
 import BookCamper from "../BookCamper/BookCamper";
+import { selectCamper } from "../../store/selectors";
 
-const CamperReviews =() =>{
-    return(
-        <div>Reviews</div>
-    )
-}
+const CamperReviews = () => {
+  const { reviews } = useSelector(selectCamper);
+  console.log(reviews);
+  const makeStars = (numberOfStars) => {
+    let stars='';
+    for (let i = 0; i < numberOfStars; i++) {
+        stars +='<span>*</span>'
+        
+    }
+    return stars;
+  };
+  return (
+    <ul>
+      {reviews.map((review) => (
+        <li key={reviews.indexOf(review)}>
+          <div>{review.reviewer_name.slice(0, 1)}</div>
+          <p>{review.reviewer_name}</p>
+          <p>{makeStars(review.reviewer_rating)}</p>
+          <p>{review.comment}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default CamperReviews;
