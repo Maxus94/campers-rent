309d59f89c83f5711872c79c72a23df30e302ce3
Maxim <kmaxusk@gmail.com>
2024-04-26 16:23:14 +0300

    fix home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,12 @@ import css from "./Home.module.css";
 const Home = () => {
   return (
     <div className={css.homepageContainer}>
+      <h1 className={css.title}>The best campers</h1>
+      <p className={css.description}>
+        We propose you service of search and rent of campers all around Ukraine.
+        Campers for vacations, weekends and business trips, for small and big
+        companies or families.
+      </p>
       <Link to="/catalog" className={css.homepageLink}>
         Book
       </Link>
